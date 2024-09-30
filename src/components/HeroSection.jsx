fff5fb9536ad97e8085462af1c004445422f8cfd
MagicGionson93 <admin-gps@unicatt.it>
2024-09-30 10:42:04 +0200

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,38 @@
+import video1 from "../assets/video1.mp4"
+import video2 from "../assets/video2.mp4"
+import video3 from "../assets/video3.mp4"
+import video4 from "../assets/video4.mp4"
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        Glow and Hydration: 
+        <span className="heroTitle">{" "}The Skincare You Deserve</span>
+      </h1>
+      <p className="mt-10 text-md text-center text-neutral-500 max-w-4xl">
+      Your skin is a reflection of your journey—nurture it with the balance and care it deserves. Every routine is a ritual, a moment of self-care, bringing you closer to the confidence that radiates from within.
+      </p>
+      <div className="flex justify-center my-10">
+        <a href="#" className="contactColor py-3 px-4 mx-3 border border-white">
+          Start for free
+        </a>
+        <a href="#" className="contactColor py-3 px-4 mx-3 border border-white">
+          Documentation
+        </a>
+      </div>
+      <div className="flex mt-10 justify-center">
+        <video autoPlay loop muted className="w-1/2 border border-white shadow-green-400 mx-2 my-4">
+          <source src={video3} type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+        <video autoPlay loop muted className="w-1/2 border border-white shadow-green-400 mx-2 my-4">
+          <source src={video4} type="video/mp4"/>
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
